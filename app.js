b5b6e5f6b6054c72c5fb0447a5385c75769064f5
riteshsute <suteritesh@gmail.com>
2023-05-16 00:25:56 +0530

    show all the messages from db

--- app.js
+++ app.js
@@ -5,18 +5,28 @@ const fs = require('fs')
 const bodyParser = require('body-parser');
 
 const cors = require('cors')
-
-app.use(cors());
+ 
+app.use(cors({
+    credentials: true
+}));
 
 const SignupRoute = require('./routes/signupRoute');
+
+const ChatMessagesRoute = require('./routes/chatMessagesRoute')
 
 const sequelize = require('./util/database');
 
 app.use(bodyParser.json({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const Chats = require('./model/chatMessages');
+const User = require('./model/signupModel');
 
-app.use(SignupRoute);
+User.hasMany(Chats);
+Chats.belongsTo(User);
+
+app.use('/ChatApp', SignupRoute);
+app.use('/ChatApp', ChatMessagesRoute)
 
 sequelize
     // .sync({ force: true })   
--- controllers/chatMessages.js
+++ controllers/chatMessages.js
@@ -1,32 +1,36 @@
-const User = require('../model/signupModel');
-const UserMessages = require('../model/chatMessages');
+const { userInfo } = require('os');
+const User = require('../model/chatMessages');
 
-const dbMessages = async (req, res) => {
-  try {
-    const { message, userId } = req.body;
-    console.log(message, userId);
-    const user = await User.findByPk(userId);
-    if (!user) {
-      throw new Error('User not found');
+
+const addMessages =  (async (req, res) => {
+    try {
+        const message = req.body.message;
+        const userId = req.body.userId;
+        console.log(userId)
+        console.log(message)
+        const user = await User.create(({ message, userId: userId }));
+
+        res.status(201).json({
+            messages: message
+    })}
+    catch (err) { 
+        res.status(500).json({success: false, message: err} )
+        console.log(err)
+      }
+})
+
+const getMessages = ( async ( req, res) => {
+    try {
+    const Message = await User.findAll()
+    res.status(200).json({allMessages: Message})
     }
-    const userMessage = await UserMessages.create({
-      message,
-      userId: user.id
-    }, {
-      include: User
-    });
-    res.status(201).json({
-      messages: userMessage
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      success: false,
-      message: err.message
-    });
-  }
-};
+    catch (err) {
+        res.status(500).json({ success: false, message: err });
+    }
+})
+ 
 
 module.exports = {
-  dbMessages
-};
+    addMessages,
+    getMessages
+}
